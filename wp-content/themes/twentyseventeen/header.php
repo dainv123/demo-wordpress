750aa623fac00cd11eeb8400460d135114df79db
dai nguyen <ngdai0402@gmail.com>
2018-08-22 00:26:16 +0700

    menu

--- wp-content/themes/twentyseventeen/header.php
+++ wp-content/themes/twentyseventeen/header.php
@@ -18,18 +18,21 @@
 <meta charset="<?php bloginfo( 'charset' ); ?>">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="profile" href="http://gmpg.org/xfn/11">
+<link href="<?php echo get_template_directory_uri(); ?>/assets_tpt/images/favicon/favicon.ico" rel="icon">
+<link href='http://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i,800,800i%7CRaleway:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i%7CUbuntu:300,300i,400,400i,500,500i,700,700i'
+	rel='stylesheet' type='text/css'>
+<link href="<?php echo get_template_directory_uri(); ?>/assets_tpt/css/external.css" rel="stylesheet">
+<link href="<?php echo get_template_directory_uri(); ?>/assets_tpt/css/bootstrap.min.css" rel="stylesheet">
+<link href="<?php echo get_template_directory_uri(); ?>/assets_tpt/css/style.css" rel="stylesheet">
+<link href="<?php echo get_template_directory_uri(); ?>/assets_tpt/css/custom.css" rel="stylesheet">
+<title>Autoshop | E-commerce Html5 Template</title>
 
-<?php wp_head(); ?>
 </head>
 
 <body <?php body_class(); ?>>
-<div id="page" class="site">
-	<a class="skip-link screen-reader-text" href="#content"><?php _e( 'Skip to content', 'twentyseventeen' ); ?></a>
-
-	<header id="masthead" class="site-header" role="banner">
-
+<div  id="wrapper" class="wrapper clearfix">
+	<header id="navbar-spy" class="header header-1">
 		<?php get_template_part( 'template-parts/header/header', 'image' ); ?>
-
 		<?php if ( has_nav_menu( 'top' ) ) : ?>
 			<div class="navigation-top">
 				<div class="wrap">
--- wp-content/themes/twentyseventeen/template-parts/header/header-image.php
+++ wp-content/themes/twentyseventeen/template-parts/header/header-image.php
@@ -9,12 +9,51 @@
  */
 
 ?>
-<div class="custom-header">
-
-		<div class="custom-header-media">
-			<?php the_custom_header_markup(); ?>
+<div class="top-bar">
+	<div class="container">
+		<div class="row">
+			<div class="col-xs-12 col-sm-6 col-md-5">
+				<ul class="list-inline top-contact">
+					<li>
+						<span>Phone :</span> + 2 0106 5370701</li>
+					<li>
+						<span>Email :</span> 7oroof@7oroof.com</li>
+				</ul>
+			</div>
+			<div class="col-xs-12 col-sm-6 col-md-7">
+				<ul class="list-inline pull-right top-links">
+					<li>
+						<a href="#">Login</a>
+						/
+						<a href="#">Register</a>
+					</li>
+					<li>
+						<a href="#">Wishlist</a>
+					</li>
+					<li>
+						<a href="#">Checkout</a>
+					</li>
+					<li>
+						<a href="#">Stores</a>
+					</li>
+					<li class="dropdown">
+						<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">English
+							<i class="fa fa-angle-down"></i>
+						</a>
+						<ul class="dropdown-menu">
+							<li>
+								<a href="#">Arabic</a>
+							</li>
+							<li>
+								<a href="#">German</a>
+							</li>
+						</ul>
+					</li>
+				</ul>
+			</div>
 		</div>
-
-	<?php get_template_part( 'template-parts/header/site', 'branding' ); ?>
-
-</div><!-- .custom-header -->
+		<!-- .row end -->
+	</div>
+	<!-- .container end -->
+</div>
+<!-- .top-bar end -->
